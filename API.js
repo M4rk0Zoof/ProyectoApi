fe2e1b5152eeb3cc08cc0be93dc30fe897f61b6e
Marcos <166460088+M4rk0Zoof@users.noreply.github.com>
2024-11-11 19:11:34 -0300

    Add files via upload

--- API.js
+++ API.js
@@ -15,14 +15,33 @@ Arianna();
 objetos = [
     {url:"https://api.disneyapi.dev/character/6",
     color:"verde",
-    descripcion:"Una tortuga"
-    },
+    descripcion:"Una tortuga"},
     {url:"https://api.disneyapi.dev/character/7",
     color:"azul",
-    descripcion:"No sé"
-    },
+    descripcion:"No sé"},
     {url:"https://api.disneyapi.dev/character/10",
     color:"azul",
-    descripcion:"No sé"
+    descripcion:"No sé"}
+];
+
+function personaje(){
+    const node = document.createElement("p");
+    const descrip= document.createElement("p");
+    const fota = document.createElement("img");
+    async function datos(){
+        var respuesta = await fetch("https://api.disneyapi.dev/character/308");
+        var personaje = await respuesta.json();
+        const textnode = document.createTextNode(personaje.data.name);
+        node.appendChild(textnode);
+        const descnode = document.createTextNode("Una reina.");
+        descrip.appendChild(descnode);
+        fota.src = personaje.data.imageUrl;
+        fota.className = "foto";
+        fota.width = 100;
+        fota.height = 100;
     }
-]
+    datos();
+    document.getElementById("nombre").appendChild(node);
+    document.getElementById("descripzinha").appendChild(descrip);
+    document.getElementById("fotinha").appendChild(fota);
+}
