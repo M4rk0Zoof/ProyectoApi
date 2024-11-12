2bc8a600f93ed641ac5d2bcfceb83a2647ef9dcb
Marcos <166460088+M4rk0Zoof@users.noreply.github.com>
2024-11-11 21:32:15 -0300

    Add files via upload

--- API.js
+++ API.js
@@ -31,43 +31,43 @@ objetos = [
 ];
 
 function personaje(){
-    const node = document.createElement("p");
-    const descrip= document.createElement("p");
-    const fota = document.createElement("img");
+    const nombre = document.createElement("p");
+    const descripcion = document.createElement("p");
+    const foto = document.createElement("img");
     async function datos(){
         var respuesta = await fetch("https://api.disneyapi.dev/character/308");
         var personaje = await respuesta.json();
-        const textnode = document.createTextNode(personaje.data.name);
-        node.appendChild(textnode);
-        const descnode = document.createTextNode("Una reina.");
-        descrip.appendChild(descnode);
-        fota.src = personaje.data.imageUrl;
-        fota.className = "foto";
-        fota.width = 100;
-        fota.height = 100;
+        const textnombre = document.createTextNode(personaje.data.name);
+        nombre.appendChild(textnombre);
+        const textdescripcion = document.createTextNode("Una reina.");
+        descripcion.appendChild(textdescripcion);
+        foto.src = personaje.data.imageUrl;
+        foto.className = "foto";
+        foto.width = 100;
+        foto.height = 100;
     }
     datos();
-    document.getElementById("nombre").appendChild(node);
-    document.getElementById("descripzinha").appendChild(descrip);
-    document.getElementById("fotinha").appendChild(fota);
+    document.getElementById("nombre").appendChild(nombre);
+    document.getElementById("descripzinha").appendChild(descripcion);
+    document.getElementById("fotinha").appendChild(foto);
 }
 
 function tarjeta(){
-    const dividid = document.getElementById('contenedor-izquierda');
-    const tarje = document.createElement("div");
-    tarje.className = "tarjeta";
-    dividid.appendChild(tarje);
+    const div = document.getElementById('contenedor-izquierda');
+    const tarjeta = document.createElement("div");
+    tarjeta.className = "tarjeta";
+    div.appendChild(tarjeta);
     const foto = document.createElement("img");
     foto.className = "foto";
-    tarje.appendChild(foto);
-    const nome = document.createElement("p");
-    nome.className = "nombre";
-    const nomee = document.createTextNode("Nombre del personaje.");
-    nome.appendChild(nomee);
-    tarje.appendChild(nome);
-    const descri = document.createElement("p");
-    descri.className = "descripcion";
-    const descrii = document.createTextNode("Descripción.");
-    descri.appendChild(descrii);
-    tarje.appendChild(descri);
+    tarjeta.appendChild(foto);
+    const nombre = document.createElement("p");
+    nombre.className = "nombre";
+    const nombretxt = document.createTextNode("Nombre del personaje.");
+    nombre.appendChild(nombretxt);
+    tarjeta.appendChild(nombre);
+    const descripcion = document.createElement("p");
+    descripcion.className = "descripcion";
+    const descripciontxt = document.createTextNode("Descripción.");
+    descripcion.appendChild(descripciontxt);
+    tarjeta.appendChild(descripcion);
 }
